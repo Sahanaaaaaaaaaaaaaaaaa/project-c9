1ae302d79a6d50ccb5ff5f057eb7f03eaf38086d
sahana <86104885+Sahanaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2021-06-25 10:02:33 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var btn_red;
+var btn_green
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400);  
+   btn_red = createButton("Red");
+   btn_red.position(150,100); 
+   btn_red.mousePressed(red_bg); 
+  
+   btn_green = createButton("Green"); 
+   btn_green.poistion(200,100); 
+   btn_green.mousePressed(green_bg); 
+  
+}
+
+function draw() {
+  background(r,g,b);
+ 
+}
+
+function red_bg() 
+ { 
+  r = 225;
+  g = 0;
+  b = 0; 
+}
+function green_bg()
+ {r = 0;
+  g = 225;
+  b = 0;
+}
